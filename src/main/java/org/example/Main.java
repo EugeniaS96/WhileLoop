9eb2b3683ca3579e01a9232c3a08d63b051a5eea
Eugenia <geniya1994@mail.ru>
2025-02-14 16:00:30 +0300

    new commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите целое число: ");
+        int number = scanner.nextInt();
+
+        for (int i = 0; i <= number; i++) {
+            if (i == 0) {
+                System.out.println(0); // Выводим 0 как есть
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+        scanner.close();
+    }
+}
